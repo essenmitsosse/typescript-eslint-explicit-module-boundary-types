5951beaea453f802a172cb41961d82e8b4789d61
essenmitsosse <marcus@essenmitsosse.de>
2020-06-04 10:07:30 +0200

    Improve formatting

--- index.ts
+++ index.ts
@@ -1,9 +1,18 @@
-export const foo = (): (n: number) => (m: number) => string => (n) => (m) => String(n+m)
+export const foo = 
+    (): (n: number) => (m: number) => string => 
+        (n) => 
+            (m) => 
+                String(n+m)
 
 export const bar: 
-() => (n: number) => string 
-= () => (n) => String(n)
+() => (n: number) => string = 
+    () => 
+        (n) => 
+            String(n)
 
 type Buz = () => (n: number) => string 
 
-export const buz: Buz = () => (n) => String(n)
+export const buz: Buz = 
+    () => 
+        (n) => 
+            String(n)
